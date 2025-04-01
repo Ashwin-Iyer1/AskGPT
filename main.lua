275b77d1564ce8f7f516dbd1a2b90d263f70ab6e
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2025-04-01 10:25:32 -0700

    add button on word hold

--- main.lua
+++ main.lua
@@ -2,9 +2,12 @@ local Device = require("device")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local NetworkMgr = require("ui/network/manager")
 local _ = require("gettext")
+local UIManager = require("ui/uimanager")
+local queryChatGPT = require("gpt_query")
 
 local showChatGPTDialog = require("dialogs")
 local UpdateChecker = require("update_checker")
+
 
 local AskGPT = InputContainer:new {
   name = "askgpt",
@@ -32,4 +35,45 @@ function AskGPT:init()
   end)
 end
 
+function AskGPT:onDictButtonsReady(dict_popup, buttons)
+  if dict_popup.is_wiki_fullpage then
+    return
+  else
+    if buttons[2] then
+      table.insert(buttons[2], 1, {  -- Insert inside the first button group
+          id = "GPTDefinition",
+          text = _("askChat"),
+          font_bold = true,
+          callback = function()
+            NetworkMgr:runWhenOnline(function()
+              if not updateMessageShown then
+                UpdateChecker.checkForUpdates()
+                updateMessageShown = true
+              end
+              local InfoMessage = require("ui/widget/infomessage")
+              local queryWord = dict_popup.word
+              local systemDialog = {
+                role = "system",
+                content = "You are a helpful translation assistant. Provide direct translations without additional commentary."
+              }
+              local queryDialog = {systemDialog, {
+                role = "user",
+                content = "What is the definition of this word: ".. queryWord.. " given that the name of the book is: " .. self.ui.document:getProps().title
+              }}
+              local loading = InfoMessage:new{
+                text = _("Loading..."),
+                timeout = 0.1
+              }
+              UIManager:show(loading)
+              UIManager:scheduleIn(0.1, function()
+                UIManager:show(InfoMessage:new{
+                  text = _(queryChatGPT(queryDialog)),
+              })
+              end)
+            end)
+          end
+      })
+    end  
+  end
+end
 return AskGPT
